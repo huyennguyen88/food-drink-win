9909b7aed2db361530e7d5c2d89cfa5f0002cf71
Nguyễn Thanh Huyền <judyhuyen17498@gmail.com>
2019-10-24 15:39:51 +0700

    Initialize repository

---
+++ src/component/TopNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+class TopNav extends React.Component {
+  render(){
+    return (
+      <div className="TopNav">
+          
+      </div>
+    );
+  }
+}
+export default TopNav;
