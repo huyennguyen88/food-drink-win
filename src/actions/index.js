1c2b9fd73b3302f6c19974b28711b5ffc3fc83a3
Nguyễn Thanh Huyền <judyhuyen17498@gmail.com>
2019-11-02 10:21:34 +0700

    no studying api

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+import * as types from '../constants/ActionTypes'
+export const allItem = ()=>{
+    return {
+        type: types.ALL_ITEM
+    }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import items from "./items";
+const myReducer = combineReducers({
+    items
+});
+export default myReducer;
