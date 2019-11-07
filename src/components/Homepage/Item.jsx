fdac549d150cf5ccc431b34416ace29914403818
ueihgnurt <30955938+ueihgnurt@users.noreply.github.com>
2019-11-07 20:48:16 +0700

    show and detail complete (#7)

--- src/components/Homepage/Item.jsx
+++ src/components/Homepage/Item.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import {
-    Link
+    Link,
+    useParams
 } from 'react-router-dom';
 
 class Item extends React.Component {
-
+    
     render() {
-
-        return (
+            return (
     
                 <div className="Item col-lg-3 col-md-6 mb-4">
                     <div className="card h-100 border-warning">
-                        <Link to="/item" ><img className="card-img-top" src={this.props.name} alt="" /></Link>
+                        <Link to={"/products/"+this.props.id}><img className="card-img-top" src={this.props.img} alt="" /></Link>
                         <div className="card-body">
                             <h4 className="card-title ">
-                                <a className="text-success" href="abc">Item One</a>
+                                <a className="text-success" href="abc">{this.props.name}</a>
                             </h4>
-                            <p className="h5 text-danger">$24.99</p>
-                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                            <p className="h5 text-danger">${this.props.price}</p>
+                            <p className="card-text">{this.props.description}</p>
                         </div>
                         <div className="card-footer bg-warning">
                             <small className="text-muted">★ ★ ★ ★ ☆</small>
@@ -26,8 +26,7 @@ class Item extends React.Component {
                 </div>
                 
   
-         
-
+        
         );
     }
 }
---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,2 @@
+export const FETCH_PRODUCTS ='FETCH_PRODUCTS';
+export const PRODUCT_SHOW = 'PRODUCT_SHOW'
